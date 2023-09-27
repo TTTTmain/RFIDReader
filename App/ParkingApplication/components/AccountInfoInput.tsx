db3b826b1e6003814f9df369a6af3b798519b47e
ttan0053 <ttan0053@student.monash.edu>
2023-09-27 22:41:26 +1000

    added default page to save account info [no backend/db]

--- App/ParkingApplication/components/AccountInfoInput.tsx
+++ App/ParkingApplication/components/AccountInfoInput.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { StyleSheet } from 'react-native';
+import React, { useState } from 'react';
+import { Button, StyleSheet } from 'react-native';
 
 import Colors from '../constants/Colors';
 import { ExternalLink } from './ExternalLink';
@@ -9,18 +9,46 @@ import { Text, TextInput } from 'react-native';
 
 
 export default function AccountInfoInput({ path }: { path: string }) {
+  const [accountId, setAccountId] = useState('');
+  const [plateId, setPlateId] = useState('');
   return (
     <View>
-      <Text> stuff </Text>
-      <TextInput />
+      <View style={styles.getStartedContainer}>
+        <Text> Account ID </Text>
+        <TextInput style={styles.inputBox}
+         onChangeText={accountId => setAccountId(accountId)}
+        />
+      </View>
+      <View style={styles.getStartedContainer}>
+        <Text> Liscence Plates </Text>
+        <TextInput style={styles.inputBox}
+          onChangeText={plateId => setPlateId(plateId)}
+        />
+      </View>
+      <Button
+        onPress={submitAccountInfo}
+        title="Submit"
+      />
     </View>
   );
 }
 
+function submitAccountInfo() {
+
+}
+
 const styles = StyleSheet.create({
+  inputBox: {
+    borderColor: '#5A5A5A',
+    borderWidth: 1,
+    width: 100,
+    borderRadius: 5,
+    textAlign: 'center'
+  },
   getStartedContainer: {
     alignItems: 'center',
     marginHorizontal: 50,
+    paddingBottom: 20
   },
   homeScreenFilename: {
     marginVertical: 7,
